4d89427e311a368df5f4d8bbc05358ecbb303514
kaushalluffa <kaushalluffa91@gmail.com>
2024-02-25 22:09:00 -0500

    fixed socket io auth middleware

--- src/middleware/ioMiddleware.ts
+++ src/middleware/ioMiddleware.ts
@@ -1,17 +1,18 @@
-import jwt from "jsonwebtoken";
-import { JWT_SECRET_KEY } from "../constants";
+import { secret } from "../constants";
 import { Socket } from "socket.io";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
 import { ExtendedError } from "socket.io/dist/namespace";
-export default function ioMiddleware(
+import { jwtVerify } from "jose";
+export default async function ioMiddleware(
   socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
   next: (err?: ExtendedError | undefined) => void
 ) {
-  const token = socket.handshake.headers.authorization ?? "";
+  const token = socket.handshake.auth.token ?? "";
+  let verifiedToken;
   try {
-    const decoded: any = jwt.verify(token, JWT_SECRET_KEY); // Replace with your actual secret
-    const now = Date.now() / 1000;
-    if (now > decoded?.exp) {
+    verifiedToken = await jwtVerify(token, secret);
+
+    if (!verifiedToken) {
       const err = new Error("Token expired");
       next(err);
     }
