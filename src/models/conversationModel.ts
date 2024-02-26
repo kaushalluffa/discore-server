9ed104d4f5f9f11061b5ad5e395d0032624261de
kaushalluffa <kaushalluffa91@gmail.com>
2024-02-25 22:08:30 -0500

    updated conversation model for convo crud

--- src/models/conversationModel.ts
+++ src/models/conversationModel.ts
@@ -1,11 +1,11 @@
 import { prisma } from "../prisma";
 export const createConversation = async (members: any) => {
   try {
-    const conversation = prisma.conversation.create({
+    const conversation = await prisma.conversation.create({
       data: {
         type: "DIRECT_MESSAGE",
         members: {
-          create: members,
+          create: members?.map((user: any) => ({ userId: user?.id })),
         },
       },
       include: { members: true },
@@ -16,13 +16,24 @@ export const createConversation = async (members: any) => {
     console.log(error);
   }
 };
-export const getConversations = async (id: any) => {
+export const getConversations = async (userId: any) => {
   try {
     const conversations = await prisma.conversation.findMany({
-      where: { members: { some: { userId: id } } },
-      include: { members: true },
+      where: { members: { some: { userId } } },
+      include: { members: { include: { user: true } } },
     });
     return conversations;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getOneConversation = async (conversationId: any) => {
+  try {
+    const oneConversation = await prisma.conversation.findFirst({
+      where: { id: conversationId },
+      include: { members: { include: { user: true } } },
+    });
+    return oneConversation;
   } catch (error) {
     console.log(error);
   }
