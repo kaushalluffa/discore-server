a9f76fb2b6cb0223b8c896b3339561cbfd16a67c
kaushalluffa <kaushalluffa91@gmail.com>
2024-03-29 14:45:11 -0400

    cors fix

--- src/models/authModel.ts
+++ src/models/authModel.ts
@@ -31,7 +31,7 @@ export const signup = async (req: Request, res: Response) => {
         secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true,
-        domain:'.netlify.app'
+        domain: ".onrender.com",
       });
       return res.json({
         message: "Signed up successfully",
@@ -76,7 +76,7 @@ export const login = async (req: Request, res: Response) => {
         secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true,
-        domain: ".netlify.app",
+        domain: ".onrender.com",
       });
       return res.json({
         message: "Logged in successfully",
